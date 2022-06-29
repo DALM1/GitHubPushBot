c3286e6ef02a41b461dd00f04fffbb284cc381c8
Dimitri ALMON <dalm1@MacBook-Air-de-Dimitri.local>
2022-06-29 03:02:47 +0200

    commit

---
+++ main.py
@@ -0,0 +1,15 @@
+import os
+from random import randint
+
+for i in range(1, 365):
+
+    for j in range(0, randint(1, 10)):
+        d = str(i) + ' Days ago'
+        with open('file.txt', 'a') as file:
+            file.write(d)
+            os.system('git add .')
+            os.system('git commit --date="' + d + '" -m "commit"')
+
+os.system('git push -u origin main')
+
+    
